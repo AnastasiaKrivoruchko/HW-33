30bd47afc31035eef651a95513207aff407cfe93
Anastasia <anastasia.krivoruchko.work@gmail.com>
2023-03-22 17:59:25 +0200

    create table

---
+++ script.js
@@ -0,0 +1,18 @@
+const table = document.createElement('table');
+let number = 1;
+
+for (let i = 0; i < 10; i++) {
+    const tr = document.createElement('tr');
+
+    for (let j = 0; j < 10; j++) {
+        const td = document.createElement('td');
+        td.innerHTML = number;
+        tr.append(td);
+
+        number++;
+    }
+
+    table.append(tr);
+}
+
+document.body.append(table);
